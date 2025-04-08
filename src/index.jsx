a989ebc5b771520ec19411d837b7ac909839c0ef
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-08 09:34:25 -0300

    feat: config reducers

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-
+import { createRoot } from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './main/App'
+import reducers from './main/reducers'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const store = createStore(reducers)
+const root = createRoot(document.getElementById('app'))
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
